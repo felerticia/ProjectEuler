4b26d14562b42fee0a84f0c112969d2b928e061d
felerticia <felerticia@gmail.com>
2023-02-13 09:36:50 +0100

    Add files via upload

--- 044.js
+++ 044.js
@@ -1,39 +1,35 @@
-function nextPermutation(arr) {
-    let i = arr.length - 1;
-    while (i > 0 && arr[i - 1] >= arr[i])
-        i--
+    let minD = -1; 
+    let i=2
+    while (true){
+        let pentI = pentagonalNumber(i);
+        if (minD != -1 && pentI - pentagonalNumber(i - 1) >= minD)
+            break;
+        
+        for (let j = i - 1; j >= 1; j--) {
+            let pentJ = pentagonalNumber(j);
+            let diff = pentI - pentJ;
+            if (minD != -1 && diff >= minD)
+                break;
+            else if (isPentagonalNumber(pentI + pentJ) && isPentagonalNumber(diff))
+                minD = diff;  
+        }
+        i++
+    }
+    console.log(minD);
 
-    if (i <= 0)
+
+
+function pentagonalNumber(x) {
+    return x * (x * 3 - 1) / 2;
+}
+
+
+function isPentagonalNumber(y) {
+    if (y <= 0)
         return false;
+    
 
-    let j = arr.length - 1;
-    while (arr[j] <= arr[i - 1])
-        j--
-   
-    [arr[i-1],arr[j]] = [arr[j],arr[i-1]]
-
-    for (let k = arr.length - 1; k > i; i++, k--) 
-        [arr[i],arr[k]] = [arr[k],arr[i]]
-
-    return true;
+    let temp = y * 24 + 1;
+    let sqrt = Math.sqrt(temp);
+    return sqrt * sqrt == temp && sqrt % 6 == 5;
 }
-
-let sum = 0
-let digits = new Array(10).fill().map((x,i) => i)
-
-while (nextPermutation(digits)){
-    if (
-        Number(digits.slice(1,4).join('')) % 2 === 0 &&
-        Number(digits.slice(2,5).join('')) % 3 === 0 &&
-        Number(digits.slice(3,6).join('')) % 5 === 0 &&
-        Number(digits.slice(4,7).join('')) % 7 === 0 &&
-        Number(digits.slice(5,8).join('')) % 11 === 0 &&
-        Number(digits.slice(6,9).join('')) % 13 === 0 &&
-        Number(digits.slice(7,10).join('')) % 17 === 0 
-    )
-
-    sum += Number(digits.join(''))
-}
-
-console.log(sum);
-
